a5d24ad5cc5e13d67be68ce3e93f07369b2ae387
Олег Сахарук <saharukoleg1@gmail.com>
2020-04-27 20:42:34 +0300

    first commit

---
+++ src/main/java/com/appsdeveloperblog/app/ws/MobileAppWsdemoApplication.java
@@ -0,0 +1,14 @@
+package com.appsdeveloperblog.app.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileAppWsdemoApplication {
+
+//test
+	public static void main(String[] args) {
+		SpringApplication.run(MobileAppWsdemoApplication.class, args);
+	}
+
+}
