6ddc1e7b863413dd0bf322feb89c72c60a339bb7
Олег Сахарук <saharukoleg1@gmail.com>
2020-04-28 15:43:31 +0300

    Making Parameter Optional or Required (required badly work with primitives) if in URL you didn't added some value you will see default value

--- src/main/java/com/appsdeveloperblog/app/ws/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/controller/UserController.java
@@ -7,9 +7,10 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @GetMapping()
-    public String getUsers(@RequestParam(value = "page") int page,
-                           @RequestParam(value = "limit") int limit) {
-        return "get users was called with page = " + page + " and limit = " + limit;   // for this endpoint we can send users/?page=1&limit=33 in URL
+    public String getUsers(@RequestParam(value = "page", defaultValue = "1") int page,
+                           @RequestParam(value = "limit", defaultValue = "50") int limit,
+                           @RequestParam(value = "sort", defaultValue = "desc", required = false) String sort) {
+        return "get users was called with page = " + page + " and limit = " + limit + " and sort = " + sort;
     }
 
     @GetMapping(path = "/{userId}")
