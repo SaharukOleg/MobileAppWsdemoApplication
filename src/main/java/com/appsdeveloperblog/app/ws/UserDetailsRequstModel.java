96a00ed88bf55cfd48e24fb2b75d4c04b9b7eeae
Олег Сахарук <saharukoleg1@gmail.com>
2020-06-01 17:36:03 +0300

    Validating HTTP POST Request Body (added annotation over the fields)

--- src/main/java/com/appsdeveloperblog/app/ws/UserDetailsRequstModel.java
+++ src/main/java/com/appsdeveloperblog/app/ws/UserDetailsRequstModel.java
@@ -1,10 +1,22 @@
 package com.appsdeveloperblog.app.ws;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class UserDetailsRequstModel {
 
+    @NotNull(message = "first name cannot be null")
+    @Size(min = 2, message = "First name must not be less than 2 characters")
     private String firstName;
+    @NotNull(message = "last name cannot be null")
+    @Size(min = 2, message = "First name must not be less than 2 characters")
     private String lastName;
+    @NotNull(message = "email cannot be null")
+    @Email
     private String email;
+    @NotNull(message = "password cannot be null")
+    @Size(min = 8, max = 16, message = "Password must be equal or grater than 8 characters and less than 16 characters")
     private String password;
 
     public String getFirstName() {
--- src/main/java/com/appsdeveloperblog/app/ws/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 
 @RestController
 @RequestMapping("/users")
@@ -41,7 +43,7 @@ public class UserController {
             MediaType.APPLICATION_XML_VALUE,
             MediaType.APPLICATION_JSON_VALUE
     })
-    public ResponseEntity<UserRest> createUser(@RequestBody UserDetailsRequstModel userDetails) {
+    public ResponseEntity<UserRest> createUser(@Valid @RequestBody UserDetailsRequstModel userDetails) {
 
         UserRest userRest = new UserRest();
         userRest.setEmail(userDetails.getEmail());
